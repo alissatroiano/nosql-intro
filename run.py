99ec4edb2ff6cb67be5ce0da7d274a10680381b0
Alissa Troiano <alissatroianonyc@gmail.com>
2020-10-24 18:53:31 +0000

    Use delete where in method to delete data from table

--- run.py
+++ run.py
@@ -1,24 +1,24 @@
 import os
-import datetime
 import pymysql
 
-# Get username from Gitpod workspace
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
+username = os.getenv('C9_USER')
 
-username = os.getenv('alissatroiano')
-
+# Connect to the database
 connection = pymysql.connect(host='localhost',
-                            user=username,
-                            password='',
-                            db='Chinook')
+                             user=username,
+                             password='',
+                             db='Chinook')
 try:
-    # Run a query here
     with connection.cursor() as cursor:
-        rows = [("Bob", 30, "1990-02-06 23:04:56"),
-                ("Jim", 56, "1955-05-09 13:12:45"),
-                ("Fred", 100, "1920-09-12 01:01:01")]
-        cursor.executemany("INSERT INTO Friends VALUES(%s, %s, %s);", rows)
-        connection.commit()
+        list_of_names = ['fred', 'Fred']
+        # Prepare string with same number of placeholders as list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute(
+            "DELETE FROM Friends WHERE name in ({});".format(format_strings),
+            list_of_names)
 
+        connection.commit()
 finally:
-    # Close the connection
     connection.close()
